95534fff65998efb3f64e2a5600e8399dcf111d8
Nils Wrenger <nils@wrenger.net>
2025-08-23 23:12:45 +0200

    :wrench: Again minor fix

--- view/src/routes/lobby/LobbyOpen.svelte
+++ view/src/routes/lobby/LobbyOpen.svelte
@@ -136,7 +136,7 @@
 		fluid
 		composite
 		classes="h-full"
-		contentClasses="h-[calc(100%-61px)]"
+		contentClasses="h-[calc(100%-53px)]"
 		listClasses="preset-tonal backdrop-blur-lg pt-2 px-2 !mb-0 rounded-md whitespace-nowrap"
 	>
 		{#snippet list()}
